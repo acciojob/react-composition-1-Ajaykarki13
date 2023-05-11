6778a0f90cb6a0860cc76e40dd54dee596d8fd39
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-05-11 09:59:42 +0000

    ans

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,19 +3,14 @@ import React , {useState} from "react";
 function Tabs({tabs})
 {
     const[tab,setTab] = useState(null)
-    function handleClick(index)
-    {
-        if(index){
-        setTab(t.content)
-        }
-    }
+   
 
     return(
         <>
         <ul>
             {
             tabs.map((t,i)=>(
-          <li key={i} onClick={() => handleClick(i)}>{t.title}</li>
+          <li key={i} onClick={() => setTab(t.content)}>{t.title}</li>
             ))
 }
         </ul>
